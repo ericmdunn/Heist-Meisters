2f5535dcca298f09bf78077542d92b4f451e22f2
ericmdunn <ericmdunn@gmail.com>
2021-02-17 13:04:05 -0500

    Guards following preset paths now

--- project/NPCs/Guard.gd
+++ project/NPCs/Guard.gd
@@ -1,13 +1,14 @@
 extends "res://NPCs/PlayerDetection.gd"
 
 
-onready var navigation = get_tree().get_root().find_node("Navigation2D", true, false)
-onready var destinations = navigation.get_node("Destinations")
+# This line is from the earlier iteration of make_path(), explained below above the old make_path() code
+#onready var navigation = get_tree().get_root().find_node("Navigation2D", true, false)
+onready var destinations = get_node("Destinations")
 
-
+var direction = 1
 var motion
 var possible_destinations
-var path 
+var path = []
 
 
 export var minimum_arrival_distance = 5
@@ -48,20 +49,28 @@ func update_path():
 		$Timer.start()
 	elif path.size() > 1:
 		path.remove(0)
-# This is how Yann did it in the class. I did it the way above; seems to work functionally the same.
-#	if path.size() == 1:
-#		if $Timer.is_stopped():
-#			$Timer.start()
-#	else:
-#		path.remove(0)
 
 
 func make_path():
-	var random_number = randi() % possible_destinations.size()
-	print(random_number)
-	var new_destination = possible_destinations[random_number]
-	path = navigation.get_simple_path(position, new_destination.position, false)
-	print(path)
+	if direction == 1:
+		for destination in possible_destinations:
+			path.append(destination.position)
+		direction = -1
+		print(path)
+	else:
+		for destination in range (possible_destinations.size(), 0):
+			path.append(possible_destinations[destination].position)
+		direction = 1
+		print(path)
+
+# This is the old make_path which runs on random locations and uses
+# get_smple_path(), which doesn't work very well. New make_path is above
+#func make_path():
+#	var random_number = randi() % possible_destinations.size()
+#	print(random_number)
+#	var new_destination = possible_destinations[random_number]
+#	path = navigation.get_simple_path(position, new_destination.position, false)
+#	print(path)
 
 
 func _on_Timer_timeout():
